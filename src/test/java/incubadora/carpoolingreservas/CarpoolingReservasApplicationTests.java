71da8e6b54e4fd431f43c481a6dc328ece6d5ff1
semartinl <semartinl@alumnos.unex.es>
2024-12-26 12:59:51 +0100

    Añadido proyecto SpringBoot sobre gestión de reservas de trayectos por usuarios

---
+++ src/test/java/incubadora/carpoolingreservas/CarpoolingReservasApplicationTests.java
@@ -0,0 +1,13 @@
+/*package incubadora.carpoolingreservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolingReservasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}*/
